35dfdef72910027e1d90c4790f00d1b62efbffd6
Austin Hewlett <aust.hewlett@gmail.com>
2017-02-07 08:28:52 -0700

    basic trending search

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
 /**
  * Created by austinh on 2/7/17.
  */
+
+var host = "http://api.giphy.com";
+var api_key = "dc6zaTOxFJmzC";
+
+
+$(document).ready(function () {
+    getTrendingGif();
+});
+
+function getTrendingGif() {
+    var endpoint = "/v1/gifs/trending";
+    var url = host + endpoint + "?api_key=" + api_key + "&rating=g";
+    $.ajax({
+        url: url,
+        success: function (response) {
+            var random = Math.floor(Math.random() * 26);
+            $('#trending-gif-container').attr('src', response.data[random].images.downsized_large.url)
+        }
+    })
+}
